2805ce4991823ad35109caeab13008c8f02dd719
Miguel De leon <miguel4575@Me.com>
2022-06-18 01:06:29 -0400

    Add files via upload

--- nltk_download_utils.py
+++ nltk_download_utils.py
@@ -1,4 +1,5 @@
-import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('all-corpora')
+import cmath
+import subprocess
+cmd = ['python3','-m',' nltk.downloader words']
+subprocess.run(cmd)
+print("working")
